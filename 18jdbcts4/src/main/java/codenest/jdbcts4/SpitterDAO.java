6c04864db0f0c479d0786c62c711c9b89b3d78a5
unknown <879651072@qq.com>
2019-01-15 19:55:20 +0800

    使用NamedParameterJdbcDaoSupport和阿里DruidDataSource进行数据库操作

---
+++ 18jdbcts4/src/main/java/codenest/jdbcts4/SpitterDAO.java
@@ -0,0 +1,20 @@
+package codenest.jdbcts4;
+
+import java.util.List;
+
+public interface SpitterDAO {
+
+	public void insert(Spitter spitter);
+
+	public void batchInsert(List<Spitter> spitters);
+
+	public void deleteAll();
+
+	public Spitter getSpitterByID(long id);
+
+	public List<Spitter> getAllSpitters();
+
+	public String findNameById(int custId);
+
+	public int findIdByName(String name);
+}
---
+++ 18jdbcts4/src/main/java/codenest/jdbcts4/SpitterRowMapper.java
@@ -0,0 +1,26 @@
+package codenest.jdbcts4;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class SpitterRowMapper implements RowMapper {
+
+	@Override
+	public Object mapRow(ResultSet rs, int rowNum){
+		Spitter spitter=new Spitter();
+		try {
+			if(rs.wasNull()==false||rs.getRow()>0) {
+				spitter.setId(rs.getInt(1));
+				spitter.setName(rs.getString(2));
+				spitter.setEmail(rs.getString(3));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return spitter;
+	}
+
+}
