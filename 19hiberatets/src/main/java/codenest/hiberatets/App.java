a8da097cb1054993260df18274d0d4d301076163
unknown <879651072@qq.com>
2019-01-16 20:54:52 +0800

    Spring + Hiberate

---
+++ 19hiberatets/src/main/java/codenest/hiberatets/App.java
@@ -0,0 +1,13 @@
+package codenest.hiberatets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
